91a58f91edee1b4ad931428b64478bd5252ca63a
cifra <cifra69@gmail.com>
2018-06-12 10:55:47 +0200

    Es millora la fòrmula del càlcul de l'àrea del triangle

--- src/triangulos/Triangulo.java
+++ src/triangulos/Triangulo.java
@@ -15,7 +15,7 @@ class Triangulo extends DosDimensiones {
     }
 
     double area() {
-        return getBase() * getAltura() / 2;
+        return (getBase() * getAltura()) / 2;
     }
 
     void mostrarEstilo() {
