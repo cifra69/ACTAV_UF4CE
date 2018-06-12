b056ff1c69f0906c2130631430396656191c23ae
cifra <cifra69@gmail.com>
2018-06-12 10:32:10 +0200

    Primer commit

---
+++ src/triangulos/DosDimensiones.java
@@ -0,0 +1,31 @@
+package triangulos;
+
+//Clase para objetos de dos dimensiones
+//DosDimensiones.java
+
+class DosDimensiones {
+
+    private double base;
+    private double altura;
+
+    //Métodos de acceso para base y altura
+    double getBase() {
+        return base;
+    }
+
+    double getAltura() {
+        return altura;
+    }
+
+    void setBase(double b) {
+        base = b;
+    }
+
+    void setAltura(double h) {
+        altura = h;
+    }
+
+    void mostrarDimension() {
+        System.out.println("La base y altura es: " + base + " y " + altura);
+    }
+}
---
+++ src/triangulos/Triangulo.java
@@ -0,0 +1,24 @@
+package triangulos;
+
+//Una subclase de DosDimensiones para Triangulo
+//Triangulo.java
+
+class Triangulo extends DosDimensiones {
+
+    private String estilo;
+
+    //Constructor
+    Triangulo(String s, double b, double h) {
+        super.setBase(b);
+        super.setAltura(h);
+        estilo = s;
+    }
+
+    double area() {
+        return getBase() * getAltura() / 2;
+    }
+
+    void mostrarEstilo() {
+        System.out.println("Triangulo es: " + estilo);
+    }
+}
